a8d87d30b8ccd35d2f9251d7931dad6fd654a826
Rajab-Alkahef <rajabalkahef3@gmail.com>
2024-10-20 11:32:40 +0300

    add cached network image and use it

--- lib/features/home/presentation/view/widgets/cutsom_book_image_item.dart
+++ lib/features/home/presentation/view/widgets/cutsom_book_image_item.dart
@@ -1,3 +1,5 @@
+import 'package:bookly/core/widgets/custom_error_widget.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
@@ -5,18 +7,39 @@ class CustomBookImage extends StatelessWidget {
   final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.8 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          // color: Colors.red,
-          image: DecorationImage(
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+          aspectRatio: 2.8 / 4,
+          child: CachedNetworkImage(
             fit: BoxFit.fill,
-            image: NetworkImage(imageUrl),
+            placeholder: (context, url) => Container(
+              color: const Color.fromARGB(255, 45, 0, 77),
+              child: const Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+            errorWidget: (context, url, error) {
+              return Container(
+                color: const Color.fromARGB(255, 45, 0, 77),
+                child: const CustomErrorWidget(
+                  errMessage: "Oops there was an error while loading image!",
+                ),
+              );
+            },
+            imageUrl: imageUrl,
+          )
+          //  Container(
+          //   decoration: BoxDecoration(
+          //     borderRadius: BorderRadius.circular(16),
+          //     // color: Colors.red,
+          //     image: DecorationImage(
+          //       fit: BoxFit.fill,
+          //       image: NetworkImage(imageUrl),
+          //     ),
+          //   ),
+          // ),
           ),
-        ),
-      ),
     );
   }
 }
